fb501c8c9e4759a05331c90a7746fb7e53e2aaf1
johan <johan.philipsson@hotmail.com>
2016-08-30 21:01:08 +0200

    Created bashrc

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 # Remove old config files
+rm ~/.bashrc
 rm ~/.vimrc
 rm -rf ~/.vim 2> /dev/null
 
 # Create links to repo
+ln -s $(pwd)/.bashrc ~/.bashrc
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
 
