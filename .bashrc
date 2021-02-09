ad6ae3ce37278bbbacaceff96d7d2591fe33f376
Johan Philipsson <johan.philipsson@hotmail.com>
2021-02-09 17:50:52 +0100

    Add prompt to bashrc

--- .bashrc
+++ .bashrc
@@ -7,3 +7,10 @@ alias la='ls -la'
 alias ..='cd ..'
 alias ...='cd ..; cd ..'
 alias ....='cd ..; cd ..; cd ..'
+
+
+# Set Command Prompt
+parse_git_branch() {
+     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
+export PS1="[\[\e[32m\]\A\[\e[m\] \w] \[\e[31m\]\$(parse_git_branch)\[\e[m\] \[\e[32m\]\\$\[\e[m\] "
--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Remove old config files
-rm -f ~/.bashrc
+#rm -f ~/.bashrc
 rm -f ~/.vimrc
 rm -rf ~/.vim 2> /dev/null
 
 #Create useful dirs
-mkdir -p ~/projects
+#mkdir -p ~/projects
 
 # Create links to repo
-ln -s $(pwd)/.bashrc ~/.bashrc
+cat $(pwd)/.bashrc >> ~/.bashrc
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
-ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+#ln -s $(pwd)/.tmux.conf ~/.tmux.conf
 
